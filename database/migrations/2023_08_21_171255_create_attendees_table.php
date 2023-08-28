74cdba73af73a1183f4a9df44340c2a329f659f0
VladimirFomin90 <vladimirfomin90@yandex.ru>
2023-08-28 23:43:48 +0600

    EventFactory, EventSeeder, set TimeZone Almaty

--- database/migrations/2023_08_21_171255_create_attendees_table.php
+++ database/migrations/2023_08_21_171255_create_attendees_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,12 +15,9 @@ return new class extends Migration
     {
         Schema::create('attendees', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class);
-            $table->string('name');
-            $table->text('description')->nullable();
 
-            $table->dateTime('start_time');
-            $table->dateTime('end_time');
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Event::class);
 
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\AttendeeController;
-use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('events', EventController::class);
-Route::apiResource('events.attendees', AttendeeController::class)->scoped(['attendee' => 'event']);
