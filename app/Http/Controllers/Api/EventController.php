ed786c5e05a907a56a9c69ede2c0a0595ff4232b
VladimirFomin90 <vladimirfomin90@yandex.ru>
2023-09-03 17:53:30 +0600

    make:resource EventResource, AttendeeResource

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -31,12 +32,13 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -52,7 +54,7 @@ class EventController extends Controller
                 'end_time' => 'sometimes|date|after:start_time'
             ]),
         );
-        return $event;
+        return new EventResource($event);
     }
 
     /**
